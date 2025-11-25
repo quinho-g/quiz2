41dc69622fa292d3bfa39a0a0769b1fb9cce0ac3
GitQuinho <marcos.paquito2020@gmail.com>
2025-11-25 11:14:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let soma = document.getElementById('soma')
+let contagem = 0
+let text = document.getElementById('texto-fim')
+let comentario = document.getElementById('coment')
+function quiz(atual, proxima) {
+    let sumir = document.getElementById(atual)
+    let aparecer = document.getElementById(proxima)
+    sumir.style.display = 'none'
+    aparecer.style.display = 'flex'
+}
+if (contagem == 0) {
+    text.innerText = "Nossa você não acertou nada, não me conhece muito bem!"
+}
+function certo() {
+    contagem++
+    soma.innerText = `${contagem}/5`
+    if(contagem >= 1) {
+        text.innerText = `Você consegui acertar ${contagem}, continue tentando!`
+    }
+    if(contagem ==4) {
+        text.innerText = `Você consegui acertar ${contagem}, faltou só uma!`
+    }
+    if(contagem == 5) {
+        text.innerText = `Você consegui acertar todas as ${contagem}, você realmente me conhece!`
+    }
+}
+function acabou() {
+    comentario.style.display = 'none'
+}
+
+
